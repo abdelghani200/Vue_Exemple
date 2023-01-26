eabbf874aa4e80b55d0fbed76576a3b15fedaf24
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-01-26 09:43:24 +0100

    Library installation and project preparation(Axios with Vue js)

---
+++ RestApi/src/App.vue
@@ -0,0 +1,33 @@
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+
+import axios from "axios";
+
+export default {
+  name: 'App',
+  data(){
+    return{
+
+    }
+  },
+  created(){
+    axios.get('https://restcountries.com/v2/all').then((res)=>{
+      console.log(res.data); 
+    })
+  }
+}
+
+</script>
+
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<style>
+
+
+  
+</style>
